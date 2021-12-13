601f8ab7426667c1315186e153fba9df1d43af8d
flyingbelgian <76516307+flyingbelgian@users.noreply.github.com>
2021-12-13 15:50:08 +0800

    Upload of first working version
    First working version.

---
+++ filehandler.py
@@ -0,0 +1,46 @@
+import pandas
+import utm
+from tkinter import filedialog
+
+
+class Source():
+    def __init__(self):
+        self.file = filedialog.askopenfilename(title="Select source file to be converted")
+
+    def read_csv(self):
+        """Read all coordinates from excel file and save them as a new dataframe"""
+        data = pandas.read_csv(self.file)
+        self.coords = []
+        for entry, row in data.iterrows():
+            lat_sign = 1
+            lon_sign = 1
+            if row['Lat Dir'] == 'S':
+                lat_sign = -1
+            if row['Lon Dir'] == 'W':
+                lon_sign = -1
+            id = int(row['ID'])
+            lat = (row['Lat Deg'] + row['Lat Min']/60 + row['Lat Sec']/3600) * lat_sign
+            lon = (row['Lon Deg'] + row['Lon Min']/60 + row['Lon Sec']/3600) * lon_sign
+            utm_coords = utm.project((lon,lat))
+            coord = {
+                "id": id,
+                "lat_deg": row['Lat Deg'],
+                "lat_min": row['Lat Min'],
+                "lat_sec": row['Lat Sec'],
+                "lat_dir": row['Lat Dir'],
+                "lon_deg": row['Lon Deg'],
+                "lon_min": row['Lon Min'],
+                "lon_sec": row['Lon Sec'],
+                "lon_dir": row['Lon Dir'],
+                "lat_dec": lat,
+                "lon_dec": lon,
+                "UTM_zone": str(utm_coords[0]) + utm_coords[1],
+                "UTM_east": utm_coords[2],
+                "UTM_north": utm_coords[3],
+                }
+            self.coords.append(coord)
+
+    def write_csv(self):
+        new_data = pandas.DataFrame(self.coords)
+        new_file = self.file.strip(".csv")+"_converted.csv"
+        new_data.to_csv(new_file)
---
+++ main.py
@@ -0,0 +1,6 @@
+import filehandler
+
+source = filehandler.Source()
+
+source.read_csv()
+source.write_csv()
